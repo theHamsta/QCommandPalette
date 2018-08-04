8ccf97ab0eeaa2d79ed1a1ddfee6774078717b91
stephan <stephan.lauf@yahoo.de>
2018-08-05 00:32:10 +0200

    Format example/MainWindow.cpp

--- example/MainWindow.cpp
+++ example/MainWindow.cpp
@@ -3,32 +3,32 @@
 
 #include <QAction>
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+MainWindow::MainWindow( QWidget* parent ) :
+	QMainWindow( parent ),
+	ui( new Ui::MainWindow )
 {
-    ui->setupUi(this);
-	ui->widget->commandPaletteEngine()->addActionsFromMenu(ui->menuBar);
+	ui->setupUi( this );
+	ui->widget->commandPaletteEngine()->addActionsFromMenu( ui->menuBar );
 	ui->widget->commandPaletteEngine()->addFunctionForDynamicActions(
-			[&](QString searchQuery) {
-				QAction* action1 = new QAction( "DynamicEngine1: " + searchQuery, this );
-				QAction* action2 = new QAction( "DynamicEngine1: " +searchQuery.toUpper(), this );
-				QList<QAction*> result{ action1, action2};
-				return result;
-			}
-		);
+	[&]( QString searchQuery ) {
+		QAction* action1 = new QAction( "DynamicEngine1: " + searchQuery, this );
+		QAction* action2 = new QAction( "DynamicEngine1: " + searchQuery.toUpper(), this );
+		QList<QAction*> result{ action1, action2};
+		return result;
+	}
+	);
 	ui->widget->commandPaletteEngine()->addFunctionForDynamicActions(
-			[&](QString searchQuery) {
-				QAction* action1 = new QAction( "DynamicEngine2: " +searchQuery, this );
-				QList<QAction*> result{ action1};
-				return result;
-			}
-		);
+	[&]( QString searchQuery ) {
+		QAction* action1 = new QAction( "DynamicEngine2: " + searchQuery, this );
+		QList<QAction*> result{ action1};
+		return result;
+	}
+	);
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+	delete ui;
 }
 
 void MainWindow::on_actionQuit_triggered()
