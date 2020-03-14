8da72bc48f5f29335c42f8967c3d3c08cad5757b
Stephan Seitz <stephan.seitz@fau.de>
2020-03-14 16:35:32 +0100

    Suppress warnings in FuzzyCommandPaletteEngine.hpp

--- include/FuzzyCommandPaletteEngine.hpp
+++ include/FuzzyCommandPaletteEngine.hpp
@@ -1,32 +1,32 @@
 #pragma once
 
+#include <QList>
+
 #include "AbstractCommandPaletteEngine.hpp"
 #include "MatcherBase.h"
 
 class FuzzyCommandPaletteEngine : public AbstractCommandPaletteEngine
 {
-		Q_OBJECT
-	public:
-		FuzzyCommandPaletteEngine(): AbstractCommandPaletteEngine() {}
-		FuzzyCommandPaletteEngine( FuzzyCommandPaletteEngine&& ) = default;
-		FuzzyCommandPaletteEngine( const FuzzyCommandPaletteEngine& ) = default;
-		FuzzyCommandPaletteEngine& operator=( FuzzyCommandPaletteEngine&& ) = default;
-		FuzzyCommandPaletteEngine& operator=( const FuzzyCommandPaletteEngine& ) = default;
-		virtual ~FuzzyCommandPaletteEngine() {}
+    Q_OBJECT
+  public:
+    FuzzyCommandPaletteEngine() : AbstractCommandPaletteEngine() {}
+    FuzzyCommandPaletteEngine(FuzzyCommandPaletteEngine&&)      = delete;
+    FuzzyCommandPaletteEngine(const FuzzyCommandPaletteEngine&) = delete;
+    FuzzyCommandPaletteEngine& operator=(FuzzyCommandPaletteEngine&&) = delete;
+    FuzzyCommandPaletteEngine& operator=(const FuzzyCommandPaletteEngine&) = delete;
+    virtual ~FuzzyCommandPaletteEngine() {}
 
-		virtual void addAction( QAction* action ) override;
-		virtual void addActions( QList<QAction*>& actions ) override;
-		virtual void clearActions() override;
-		inline void setMatcherOptions( const MatcherOptions& options ) {m_matcherOptions = options; }
-		inline MatcherOptions matcherOptions() const {return m_matcherOptions;}
+    virtual void addAction(QAction* action) override;
+    virtual void addActions(QList< QAction* >& actions) override;
+    virtual void clearActions() override;
+    inline void setMatcherOptions(const MatcherOptions& options) { m_matcherOptions = options; }
+    inline MatcherOptions matcherOptions() const { return m_matcherOptions; }
 
-	public slots:
-		virtual void onSearchRequest( const QString& searchQuery ) override;
+  public slots:
+    virtual void onSearchRequest(const QString& searchQuery) override;
 
-
-	private:
-		MatcherBase m_matcherBase; ///< String matcher for fuzzy search
-		MatcherOptions m_matcherOptions; ///< Options for string matcher
-		std::map<std::string, QAction*> m_stringToActionMap; ///< Map search results to corresponding QActions
-
+  private:
+    MatcherBase m_matcherBase;                             ///< String matcher for fuzzy search
+    MatcherOptions m_matcherOptions;                       ///< Options for string matcher
+    std::map< std::string, QAction* > m_stringToActionMap; ///< Map search results to corresponding QActions
 };
